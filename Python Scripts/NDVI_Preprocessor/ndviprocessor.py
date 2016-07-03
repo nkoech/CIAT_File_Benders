c527b9eca0cde15b4d66e1e1ca091bc80e75dd91
Koech Nicholas <koechnicholas@gmail.com>
2016-07-03 17:51:37 +0300

    Added methods - "mosaic_rasters", "_init_mosaic_raster", "_init_mosaic_geoprocess", "geoprocess_raster"

--- Python Scripts/NDVI_Preprocessor/ndviprocessor.py
+++ Python Scripts/NDVI_Preprocessor/ndviprocessor.py
@@ -1,5 +1,6 @@
 import os
 import arcpy
+import ntpath
 from arcpy.sa import *
 from arcpy import env
 from readjson import get_json_data
@@ -10,6 +11,8 @@ class NDVIProcessor:
     def __init__(self, first_level_key, second_level_key):
         self.first_level_key = first_level_key
         self.second_level_key = second_level_key
+        self.file_date_id = []
+        self.mos_out_ras = []
         self.tool_settings = self._get_user_parameters()
         self.src = self.tool_settings['src_dir']
         self.dir_startswith = self.tool_settings['dir_param']
@@ -19,6 +22,7 @@ class NDVIProcessor:
         self.target_ref = self.tool_settings['target_ref']
         self.clip_poly = self.tool_settings['aoi_poly']
         self.place_name = self.tool_settings['aoi_place_name']
+        self.mosaic_operation = self.tool_settings['mosaic_operation']
 
     def _get_user_parameters(self):
         """Get contents from a Json file"""
@@ -39,8 +43,13 @@ class NDVIProcessor:
                     file_path = os.path.join(source_dir, file_name).replace('\\', '/')
                     if not os.path.exists(self.dest_dir):
                         os.makedirs(self.dest_dir)
-                    current_ref, target_ref_name = self._get_spatial_ref(file_path, self.target_ref)
-                    self.geoprocess_raster(file_name, current_ref, target_ref_name, file_path)
+                    if self.mosaic_operation:
+                        self.mosaic_rasters(file_path)
+                    else:
+                        self.geoprocess_raster(file_path)
+        if self.mos_out_ras:
+            for mos_file_path in self.mos_out_ras:
+                self.geoprocess_raster(mos_file_path)
         print('RASTER PROCESSING COMPLETED SUCCESSFULLY!!!')
 
     def validate_raster(self):
@@ -52,6 +61,108 @@ class NDVIProcessor:
                     file_path = os.path.join(source_dir, file_name).replace('\\', '/')
                     self._get_spatial_ref(file_path)
                     print('Validated..... {0}'.format(file_name))
+
+    def mosaic_rasters(self, file_path):
+        """ Get rasters and stitch them together """
+        file_name = ntpath.basename(file_path)
+        if self.mosaic_operation:
+            masked_file_paths = ""
+            file_name_date = file_name.split('.')[1]
+            if self.file_date_id:
+                if file_name_date not in set(self.file_date_id):
+                    masked_file_paths = self._mosaic_geoprocessing(file_path)
+            else:
+                masked_file_paths = self._mosaic_geoprocessing(file_path)
+            for masked_file in masked_file_paths:
+                arcpy.Delete_management(masked_file)
+
+    def _mosaic_geoprocessing(self, file_path):
+        """ Stitch several rasters together """
+        masked_file_paths = []
+        file_name = ntpath.basename(file_path)
+        mos_out_ras_name = 'MOSK_' + file_name[:11] + file_name[-53:]
+        mos_out_ras_file = self.dest_dir + '/' + mos_out_ras_name
+
+        for file_paths in self._init_mosaic_raster(file_name):
+            print('Mosaic raster..... {0}'.format(mos_out_ras_name))
+            if file_paths:
+                current_ref, target_ref_name = self._get_spatial_ref(file_paths[0], self.target_ref)
+                arcpy.MosaicToNewRaster_management(file_paths, self.dest_dir, mos_out_ras_name, current_ref, '8_BIT_UNSIGNED', '', '1', 'LAST', 'FIRST')
+                self.mos_out_ras.append(mos_out_ras_file)
+                masked_file_paths.extend(file_paths)
+        return masked_file_paths
+
+    def _init_mosaic_raster(self, init_file_name):
+        """ Get files to be stitched and preprocess them """
+        file_paths = []
+        root_dir = get_directory(self.src, self.dir_startswith)
+        for source_dir in root_dir:
+            for file_name in os.listdir(source_dir):
+                if file_name.startswith(self.file_startswith) & file_name.endswith(self.file_endswith):
+                    file_name_date = file_name.split('.')[1]
+                    if file_name == init_file_name:
+                        masked_out_ras = self._init_mosaic_geoprocess(source_dir, file_name)
+                        file_paths.append(masked_out_ras)
+                        if file_name_date not in set(self.file_date_id):
+                            self.file_date_id.append(file_name_date)
+                    else:
+                        if file_name_date == init_file_name.split('.')[1]:
+                            masked_out_ras = self._init_mosaic_geoprocess(source_dir, file_name)
+                            file_paths.append(masked_out_ras)
+        yield file_paths
+
+    def _init_mosaic_geoprocess(self, source_dir, file_name):
+        """ Project and remove bad/background raster values - set to null """
+        proj_out_ras = ""
+        where_clause_val1 = "VALUE = 0"
+        file_path = os.path.join(source_dir, file_name).replace('\\', '/')
+        current_ref, target_ref_name = self._get_spatial_ref(file_path, self.target_ref)
+        try:
+            if current_ref.name != target_ref_name:
+                proj_out_ras = self._reproject_raster(current_ref, target_ref_name, file_path)  # Reproject raster
+            else:
+                raise ValueError(
+                    'Raster processing FAILED! {0} projection is similar to that of the target reference.'.format(
+                        current_ref.name))
+        except ValueError as e:
+            print (e)
+        masked_file = 'MASKED_' + file_name
+        masked_out_ras = self._raster_setnull(proj_out_ras, masked_file, where_clause_val1)  # Set values to null
+        arcpy.Delete_management(proj_out_ras)
+        return masked_out_ras
+
+    def geoprocess_raster(self, file_path):
+        """ Raster geoprocessing interface """
+        current_ref, target_ref_name = self._get_spatial_ref(file_path, self.target_ref)
+        file_name = ntpath.basename(file_path)[5:]
+        proj_out_ras = ""
+
+        if self.mosaic_operation:
+            proj_out_ras = file_path
+        else:
+            try:
+                if current_ref.name != target_ref_name:
+                    # Reproject input raster
+                    proj_out_ras = self._reproject_raster(current_ref, target_ref_name, file_path)
+                else:
+                    raise ValueError('Raster processing FAILED! {0} projection is similar to that of the target reference.'.format(current_ref.name))
+            except ValueError as e:
+                print(e)
+
+        # Convert raster values to float values
+        ndvi_out_ras = self._raster_to_float(proj_out_ras)
+        arcpy.Delete_management(proj_out_ras)
+
+        # Clip raster to area of interest
+        clip_out_ras = self. _clip_raster(ndvi_out_ras)
+        arcpy.Delete_management(ndvi_out_ras)
+
+        # Set bad raster values to null
+        where_clause_val1 = "VALUE > 1"
+        where_clause_val2 = "VALUE < -1"
+        masked_file = self.place_name + file_name[-53:]
+        masked_out_ras = self._raster_setnull(clip_out_ras, masked_file, where_clause_val1, where_clause_val2)
+        arcpy.Delete_management(clip_out_ras)
 
     def _get_spatial_ref(self, file_path, target_ref=None):
         """ Get raster spatial reference """
@@ -66,52 +177,29 @@ class NDVIProcessor:
         except IOError as (e):
             print(str(e) + ' or is invalid/corrupted. Remove the bad file and run the process again')
 
-    def geoprocess_raster(self, file_name, current_ref, target_ref_name, file_path):
-        """ Raster geoprocessing interface """
-        try:
-            if current_ref.name != target_ref_name:
-
-                # Reproject input raster
-                proj_out_ras = self._reproject_raster(file_name, current_ref, target_ref_name, file_path)
-
-                # Convert raster values to float values
-                ndvi_out_ras = self._raster_to_float(file_name, proj_out_ras)
-                arcpy.Delete_management(proj_out_ras)
-
-                # Clip raster to area of interest
-                clip_out_ras = self. _clip_raster(file_name, ndvi_out_ras)
-                arcpy.Delete_management(ndvi_out_ras)
-
-                # Set bad raster values to null
-                masked_out_ras = self._raster_setnull(file_name, clip_out_ras)
-                arcpy.Delete_management(clip_out_ras)
-            else:
-                raise ValueError('Raster processing FAILED! {0} projection is similar to that of the target reference.'.format(current_ref.name))
-        except ValueError as e:
-            print(e)
-
-    def _reproject_raster(self, file_name, current_ref, target_ref_name, file_path):
+    def _reproject_raster(self, current_ref, target_ref_name, file_path):
         """ Reproject raster """
+        file_name = ntpath.basename(file_path)
         proj_out_ras = self.dest_dir + '/PROJ_' + file_name
         print('Projecting..... {0} from {1} to {2}'.format(file_name, current_ref.name, target_ref_name))
         arcpy.ProjectRaster_management(file_path, proj_out_ras, '"' + self.target_ref + '"', '', '', '', '', current_ref)
         return proj_out_ras
 
-    def _raster_to_float(self, file_name, proj_out_ras):
+    def _raster_to_float(self, proj_out_ras):
         """ Convert integer raster values to float values """
-        print('Converting to NDVI values..... PROJ_{0}'.format(file_name))
+        print('Converting to NDVI values..... {0}'.format(ntpath.basename(proj_out_ras)))
         float_out_ras = (Raster(proj_out_ras) - 50) / 200.0
-        ndvi_out_ras = self.dest_dir + '/NDVI_' + file_name
-        print('Saving rescaled raster..... {0}'.format('NDVI_' + file_name))
+        ndvi_out_ras = self.dest_dir + '/NDVI_' + ntpath.basename(proj_out_ras)[5:]
+        print('Saving rescaled raster..... {0}'.format(ntpath.basename(ndvi_out_ras)))
         float_out_ras.save(ndvi_out_ras)
         return ndvi_out_ras
 
-    def _clip_raster(self, file_name, ndvi_out_ras):
+    def _clip_raster(self, ndvi_out_ras):
         """ Clip raster to area of interest """
-        clip_out_ras = self.dest_dir + '/CLIPPED_NDVI_' + file_name
+        clip_out_ras = self.dest_dir + '/CLIPPED_' + ntpath.basename(ndvi_out_ras)[5:]
         if self.clip_poly and self.clip_poly.endswith('.shp'):
             if arcpy.Exists(self.clip_poly):
-                print('Clipping..... {0} to {1}'.format('NDVI_' + file_name, 'CLIPPED_NDVI_' + file_name))
+                print('Clipping..... {0} to {1}'.format(ntpath.basename(ndvi_out_ras), ntpath.basename(clip_out_ras)))
                 arcpy.Clip_management(ndvi_out_ras, '#', clip_out_ras, self.clip_poly, "", 'ClippingGeometry')
             else:
                 raise ValueError('Clipping FAILED! Clipping feature class does not exist')
@@ -119,14 +207,15 @@ class NDVIProcessor:
             raise ValueError('Clipping FAILED! Clipping geometry not provided')
         return clip_out_ras
 
-    def _raster_setnull(self, file_name, clip_out_ras):
+    def _raster_setnull(self, in_ras, masked_file, where_clause_val1, where_clause_val2=None):
         """ Set raster values to null values """
-        where_clause_val1 = "VALUE > 1"
-        where_clause_val2 = "VALUE < -1"
-        masked_file = self.place_name + '_NDVI_' + file_name[-53:]
         masked_out_ras = self.dest_dir + '/' + masked_file
-        print('Removing bad NDVI values..... {0}'.format('CLIPPED_NDVI_' + file_name))
-        out_set_null = SetNull(clip_out_ras, clip_out_ras, where_clause_val1 or where_clause_val2)
+        if where_clause_val2:
+            print('Removing bad NDVI values..... {0}'.format(ntpath.basename(in_ras)))
+            out_set_null = SetNull(in_ras, in_ras, where_clause_val1 or where_clause_val2)
+        else:
+            print('Removing bad raster values..... {0}'.format(ntpath.basename(in_ras)))
+            out_set_null = SetNull(in_ras, in_ras, where_clause_val1)
         print('Saving masked raster..... {0}'.format(masked_file))
         out_set_null.save(masked_out_ras)
         return masked_out_ras
@@ -136,8 +225,8 @@ def main():
     """Main program"""
     env.overwriteOutput = True
     arcpy.CheckOutExtension("spatial")
-    first_level_key = ['src', 'dest', 'dir_startswith', 'file_startswith', 'file_endswith', 'target_reference', 'aoi_geometry', 'aoi_name']
-    second_level_key = ['src_dir', 'dest_dir', 'dir_param', 'file_start', 'file_end', 'target_ref', 'aoi_poly', 'aoi_place_name']
+    first_level_key = ['src', 'dest', 'dir_startswith', 'file_startswith', 'file_endswith', 'target_reference', 'aoi_geometry', 'aoi_name', 'mosaic']
+    second_level_key = ['src_dir', 'dest_dir', 'dir_param', 'file_start', 'file_end', 'target_ref', 'aoi_poly', 'aoi_place_name', 'mosaic_operation']
     read_file = NDVIProcessor(first_level_key, second_level_key)
     read_file.validate_raster()
     read_file.init_geoprocess_raster()
--- Python Scripts/NDVI_Preprocessor/sourcedirectory.py
+++ Python Scripts/NDVI_Preprocessor/sourcedirectory.py
@@ -3,7 +3,6 @@ import os
 
 def get_directory(src, dir_startswith):
     """ Get file root directory """
-    file_dir = []
     try:
         if src:
             for root_path, dirs, files in os.walk(src):
@@ -11,11 +10,10 @@ def get_directory(src, dir_startswith):
                     for dir_name in dirs:
                         if dir_name.startswith(dir_startswith):
                             src_dir = os.path.join(root_path, dir_name).replace('\\', '/')
-                            file_dir.append(src_dir)
+                            yield src_dir
                 else:
                     if root_path == src:
-                        file_dir.append(root_path)
-            return file_dir
+                        yield root_path
         else:
             raise ValueError('Source directory for the files is not set')
     except ValueError as e:
