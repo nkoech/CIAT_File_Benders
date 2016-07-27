c1411b75f86b137bea4d331887bf5448fe46ed88
Koech Nicholas <koechnicholas@gmail.com>
2016-07-27 13:23:11 +0300

    Added "", ""

--- Python Scripts/Spatial_Relation_Processor/trendcorrelation.py
+++ Python Scripts/Spatial_Relation_Processor/trendcorrelation.py
@@ -48,7 +48,8 @@ class TrendCorrelation:
 
     def init_geoprocess_raster(self):
         """ Initialize raster geoprocessing """
-        self.validate_data()  # Validated raster
+        cell_size = self.validate_data()  # Validated raster
+        #print(cell_size)
 
         # if not os.path.exists(self.dest_dir):
         #     os.makedirs(self.dest_dir)  # Create destination folder
@@ -57,16 +58,17 @@ class TrendCorrelation:
     def validate_data(self):
         """ Check for  invalid/corrupted data """
         prev_file_path = ''
+        ras_resolution = []
         for root_dir, file_startswith, file_endswith in self._get_source_parameters(self.data_var):
             for source_dir, file_path, file_name in get_file_location(root_dir, file_startswith, file_endswith):
                 self._validate_spatial_ref(file_path, prev_file_path)
-                prev_file_path = file_path
                 print('Validated..... {0}'.format(file_name))
-        try:
-            if len(self.place_name) != 3:
-                raise ValueError('Input value "{0}" should be made of three characters'.format(self.place_name))
-        except ValueError as e:
-            print(e)
+                cell_size = self._get_raster_resolution(file_path, ras_resolution)  # Get raster resolution
+                if cell_size:
+                    ras_resolution.append(cell_size)
+                prev_file_path = file_path
+        self._validated_place_name()  # validated area of interest name as three letter acronym
+        return ras_resolution
 
     def _get_source_parameters(self, data_var):
         """ Get files root directory """
@@ -85,22 +87,45 @@ class TrendCorrelation:
     def _validate_spatial_ref(self, file_path, prev_file_path):
         """ Get raster spatial reference """
         try:
+            current_spatial_ref = self._get_spatial_ref(file_path)
             if prev_file_path:
+                prev_spatial_ref = self._get_spatial_ref(prev_file_path)
                 try:
-                    if self._describe_spatial_ref(file_path).factoryCode == self._describe_spatial_ref(prev_file_path).factoryCode:
-                        return self._describe_spatial_ref(file_path)
+                    if current_spatial_ref.factoryCode == prev_spatial_ref.factoryCode:
+                        return current_spatial_ref
                     else:
                         raise ValueError("{} has a different spatial reference. Please correct it and run the process again".format(file_path))
                 except ValueError, e:
                     sys.exit(e)
             else:
-                return self._describe_spatial_ref(file_path)
+                return current_spatial_ref
         except IOError as (e):
             print(str(e) + ' or is invalid/corrupted. Remove the bad file and run the process again')
 
-    def _describe_spatial_ref(self, file_path):
+    def _get_spatial_ref(self, file_path):
         """ Describe raster spatial reference """
         return arcpy.Describe(file_path).spatialReference
+
+    def _get_raster_resolution(self, file_path, ras_resolution):
+        """ Get all raster files resolution """
+        cell_width = self._get_cell_width(file_path)
+        if ras_resolution:
+            if cell_width not in set(ras_resolution):
+                return cell_width
+        else:
+            return cell_width
+
+    def _validated_place_name(self):
+        """ Check place name acronym """
+        try:
+            if len(self.place_name) != 3:
+                raise ValueError('Input value "{0}" should be made of three characters'.format(self.place_name))
+        except ValueError as e:
+            print(e)
+
+    def _get_cell_width(self, file_path):
+        """ Get raster cell size """
+        return arcpy.Describe(file_path).meanCellWidth
 
     def _delete_raster_file(self, in_file):
         """ Delete extracted file """
