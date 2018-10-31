05b41ea10c757cee222eca7b9f7d6e980b941744
Koech Nicholas <koechnicholas@gmail.com>
2018-10-31 18:56:20 +0300

    Renamed "CHIRPS_Downloader" to "CHIRPS_Processor"

---
+++ Python Scripts/CHIRPS_Processor/extractdata.py
@@ -0,0 +1,19 @@
+__author__ = "Koech Nicholas"
+__copyright__ = "Copyright 2016"
+__email__ = "koechnicholas@gmail.com"
+__status__ = "draft"
+
+
+import gzip
+import os
+
+
+def extract_data(source_dir, file_path, file_name, fname_endswith):
+    """ Extract file """
+    if file_name.endswith(fname_endswith):
+        dest_file = os.path.join(source_dir, file_name[:-3]).replace('\\', '/')
+        with gzip.open(file_path, 'rb') as in_file:
+            with open(dest_file, 'wb') as out_file:
+                out_file.write(in_file.read())
+        out_file.close()
+        in_file.close()
